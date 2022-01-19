4bca57c409ff163e194b24770c8bd34246f9fd29
Carlos <csaenz@iescomercio.com>
2022-01-19 19:14:34 +0100

    inicial

---
+++ src/com/iescomercio/prog/ud02/Funciones.java
@@ -0,0 +1,204 @@
+package com.iescomercio.prog.ud02;
+
+import java.util.Scanner;
+
+public class Funciones {
+
+	/*
+	 * � Crea una funci�n llamada caracteristicaCaracter que dado un car�cter por
+	 * par�metro devuelva: o 1 si es un d�gito. o 2 si es una letra min�scula. o 3
+	 * si es una letra may�scula. o 4 si es un espacio en blanco. o 5 si es un
+	 * car�cter que no es alfanum�rico.
+	 */
+
+	public static int caracteristicaCaracter(char s) {
+
+	}
+
+	/*
+	 * � Crea una funci�n llamada convertirCaracter que dado un car�cter de tipo
+	 * letra lo convierta en may�scula si es min�scula y en min�scula si es
+	 * may�scula
+	 */
+	public static char convertirCaracter(char m) {
+
+	}
+	/*
+	 * � Crea una funci�n llamada anteriorCaracter que dados dos caracteres
+	 * cualesquiera, devuelva cu�l es el menor independientemente de si es may�scula
+	 * o min�scula.
+	 */
+
+	public static char anteriorCaracter(char a, char k) {
+
+	}
+	/*
+	 * � Crea una funci�n llamada caracter2Integer que dado un entero como
+	 * Character, devuelva el mismo entero de tipo Integer.
+	 */
+
+	public static int caracter2Integer(char c) {
+
+	}
+//Integer
+//�	Crea una funci�n string2Integer que dado un entero como String, devolver� un entero de tipo Integer. 
+
+	public static int string2Integer(String s) {
+
+	}
+
+	/*
+	 * � Crea una funci�n mayorEntero que pasando como arguments dos enteros,
+	 * devolver� el mayor de los dos (utiliza la funci�n correspondiente de
+	 * Integer).
+	 */
+	public static int mayorEntero(int num1, int num2) {
+
+	}
+
+	// String
+	/*
+	 * � Crea la funci�n compararCadenas que dadas dos cadenas por par�metro,
+	 * devolver� verdadero en caso de que sean iguales o falso en caso contrario.
+	 */
+	public static boolean compararCadenas(String cadena1, String cadena2) {
+
+	}
+	// � Crea una funci�n llamada concatenar que dadas tres cadenas, devuelva su
+	// concatenaci�n.
+
+	public static String concatenar(String cadena1, String cadena2, String cadena3) {
+
+
+	}
+
+	/*
+	 * � Crea la funci�n deletrearCadena que pasando una cadena por par�metro,
+	 * muestre cada uno de sus caracteres por consola en una misma l�nea y separados
+	 * por espacio.
+	 */
+	public static void deletrearCadena(String cadena) {
+
+
+	}
+
+	/*
+	 * � Crea la funci�n toCamelCase que pasando una cadena por par�metro, devuelva
+	 * la misma cadena en formato CamelCase comenzando con min�scula.
+	 */
+
+	public static String toCamelCase(String cadena) {
+
+
+	}
+
+	/*
+	 * � Crea la funci�n toCamelCase que reciba dos par�metros, el primer car�cter
+	 * ser� la cadena que transformaremos a camel case y el segundo par�metro ser�
+	 * un Boolean que si es verdadero har� que la funci�n devuelva la cadena en
+	 * camel case comenzando en min�scula, y si es falso comenzando en may�scula
+	 */
+	public static String toCamelCase(String cadena, boolean minuMayu) {
+
+
+	}
+
+	/*
+	 * � Crea la funci�n subcadena que reciba tres par�metros. Primero, la cadena de
+	 * la que vamos a obtener la subcadena. Segundo, el �ndice de inicio (comenzando
+	 * por 1) desde donde voy a tomar la subcadena. Tercero, el �ndice del final
+	 * donde voy a cortar la subcadena. Debe devolver la subcadena que vaya de la
+	 * posici�n inicial a la final que indican los par�metros.
+	 */
+
+	public static String subcadena(String cadena, int indice1, int indice2) {
+
+
+	}
+	/*
+	 * � Crea una funci�n llamada esMayor que reciba por par�metro dos cadenas, y
+	 * devuelva aquella que tiene m�s caracteres.
+	 */
+
+	public static String esMayor(String cadena1, String cadena2) {
+
+
+	}
+	/*
+	 * � Crea una funci�n llamada contarEspacios que reciba por par�metro una cadena
+	 * y devuelva el n�mero de espacios que contiene.
+	 */
+
+	public static int contarEspacios(String cadena) {
+
+	}
+
+	/*
+	 * � Crea una funci�n llamada contarCaracter que reciba dos par�metros de
+	 * entrada. El primer par�metro ser� un String y el segundo un Character. La
+	 * funci�n tendr� que devolver el n�mero de apariciones del Character dentro del
+	 * String.
+	 */
+
+	public static int contarCaracter(String cadena, char character) {
+
+	}
+
+	/*
+	 * � Crea una funci�n llamada invertirCadena que reciba un String como par�metro
+	 * y devuelva el mismo String invertido.
+	 */
+	public static String invertirCadena(String cadena) {
+
+	}
+
+	/*
+	 * � Crea la funci�n de nombre esPalindromo que recibe un String como par�metro
+	 * y devuelva true en caso de que sea un pal�ndromo y falso en caso contrario.
+	 */
+	public static boolean esPalindromo(String cadena) {
+
+
+	}
+	/*
+	 * � Crea la funci�n posicionLetra que dada una String como par�metro y un
+	 * Character. Devuelva la posici�n de la primera vez que aparece dicho car�cter.
+	 * Si no aparece devolver� -1.
+	 */
+
+	public static int posicionLetra(String cadena, char character) {
+
+	}
+	/*
+	 * � Crea la funci�n contieneCadena que recibir� dos par�metros de entrada. El
+	 * primero ser� la cadena donde se buscar�, el segundo ser� el par�metro que se
+	 * buscar�. La funci�n devolver� verdadero en caso de que est� contenida, y
+	 * falso en caso contrario.
+	 */
+
+	public static boolean contieneCadena(String cadena1, String cadena2) {
+	}
+
+	/*
+	 * � Crea una funci�n eliminarVocales que reciba como par�metro de entrada un
+	 * String la devuelva sin ninguna vocal.
+	 */
+
+	public static String eliminarVocales(String cadena) {
+
+	}
+	/*
+	 * � Crea una funci�n reemplazarVocales que reciba como par�metro de entrada un
+	 * String y un Character que sea vocal. La funci�n deber� devolver la misma
+	 * cadena sustituyendo cada vocal por la vocal que se pasa como par�metro.
+	 */
+
+	public static String reemplazarVocales(String cadena, char character) {
+
+	}
+
+	public static void main(String[] args) {
+
+
+	}
+}
